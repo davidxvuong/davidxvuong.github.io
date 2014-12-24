4a4ac32ff43f6300c31eef4e91eb610a9ff9d8f9
David Vuong <david.vuong@uwaterloo.ca>
2014-12-24 00:17:37 -0500

    Move navbar to the side

--- js/main.js
+++ js/main.js
@@ -2,6 +2,25 @@ var scrollOld;
 var scrollCurrent;
 
 $(function(){
+	$(window).resize(function(){
+		if ($(window).width() <= 1010){
+			$(".navbar-list").css("width", "128px");
+			$(".navbar-list, .navbar").css("height", "100%");
+			$(".navbar-list").css("padding", "0");
+			$(".navbar").css("width", "auto");
+			$(".navbar").fadeOut();
+		}
+		else {
+			$(".navbar-list, .navbar").css("height", "auto");
+			$(".navbar-list").css("width", "auto");
+			$(".navbar-list").css("padding", "0px 150px 0px 150px");
+			$(".navbar").css("width", "100%");
+			$(".center").css("padding-top", "60px");
+			$(".navbar").fadeIn();
+		}
+		
+	});
+	
 	$(window).scroll(function(){
 		scrollOld = scrollCurrent;
 		scrollCurrent = $(this).scrollTop();

4589b94a6c29cbc2e61c9b72ab85abc765455e6f
David Vuong <david.vuong@uwaterloo.ca>
2014-12-24 01:51:17 -0500

    increased width of navbar in mobile

--- js/main.js
+++ js/main.js
@@ -23,11 +23,11 @@ $(function(){
 
 function adjustPage(){
 	if ($(window).width() <= 1010){
-			$(".navbar-list").css("width", "143px");
+			$(".navbar-list").css("width", "145px");
 			$(".navbar-list, .navbar").css("height", "100%");
 			$(".navbar-list").css("padding", "0");
 			$(".navbar").css("width", "auto");
-			$(".center").css("padding", "30px 40px 30px 183px");
+			$(".center").css("padding", "30px 40px 30px 185px");
 			$("#intro").css("padding-top", "10px");
 			$(".navbar").css("font-size", "22px");
 		}

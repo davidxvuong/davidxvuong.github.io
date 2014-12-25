b3d3698ba873e00248a847617d3b3bfc9b9db899
David Vuong <david.vuong@uwaterloo.ca>
2014-12-24 20:05:32 -0500

    adjusted js code for more mobile functionality

--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
 var scrollOld;
 var scrollCurrent;
-var isMobile = false;
 var isMenuOpen = false;
+var isMobile = false;
 
 $(function(){
 	$(window).resize(function(){
-		adjustPage();
+		adjustPage(navigator.platform, "resize");
 	});
 	
 	$(window).scroll(function(){
-		if ($(window).width() > 1148){
+		if ($(window).width() > 1148 && isMobile == false ){
 			scrollOld = scrollCurrent;
 			scrollCurrent = $(this).scrollTop();
 			
@@ -41,39 +41,74 @@ $(function(){
 	});
 });
 
-function adjustPage(){
-	if ($(window).width() <= 1148){
+function adjustPage(platform, event){
+	switch (platform){
+		case "iPhone":
+		case "iPad":
+		case "iPod":
+		case "Linux armv71":
 			isMobile = true;
-			$(".navbar-list").css("width", "150px");
-			$(".navbar-list, .navbar").css("height", "100%");
-			$(".navbar-list").css("padding", "0");
-			$(".navbar").css("width", "auto");
-			$(".center").css("padding", "30px 40px 30px 30px");
-			$("#intro").css("padding-top", "10px");
-			$(".navbar").css("font-size", "20px");
-			$(".menu").css("display", "block");
-			$(".navbar").fadeOut();
-		}
-		else {
+			$(".navbar").css("overflow-y", "scroll");
+			
+			toggleCss("small");
+			if (isMenuOpen == false) {
+				$(".navbar").hide();
+			}
+			else {
+				$(".navbar").show();
+			}
+			break;
+		default:
 			isMobile = false;
-			$(".navbar-list, .navbar").css("height", "auto");
-			$(".navbar-list").css("width", "auto");
-			$(".navbar-list").css("padding", "0px 150px 0px 150px");
-			$(".navbar").css("width", "100%");
-			$(".center").css("padding", "30px 150px 30px 150px");
-			$("#intro").css("padding-top", "60px");
-			$(".navbar").css("font-size", "15px");
-			$(".menu").css("display", "none");
-			$(".menu").css("left", "0px");
-			$(".navbar").fadeIn();
-		}
+			$(".navbar").css("overflow-y", "visible");
+			if ($(window).width() <= 1148) {
+				toggleCss("small");
+				if (event == "resize") {
+					$(".navbar").fadeOut();
+				}
+				else {
+					$(".navbar").hide();
+				}
+			}
+			else {
+				toggleCss("big");
+				if (event == "resize") {
+					$(".navbar").fadeIn();
+				}
+				else {
+					$(".navbar").show();
+				}
+			}
+			break;
+	}
+}
+
+function toggleCss(screenSize) {
+	if (screenSize == "small") {
+		$(".navbar-list").css("width", "150px");
+		$(".navbar-list, .navbar").css("height", "auto");
+		$(".navbar-list").css("padding", "0");
+		$(".navbar").css("width", "auto");
+		$(".center").css("padding", "30px 40px 30px 30px");
+		$("#intro").css("padding-top", "10px");
+		$(".navbar").css("font-size", "20px");
+		$(".menu").css("display", "block");
+	}
+	else if (screenSize == "big") {
+		$(".navbar-list, .navbar").css("height", "auto");
+		$(".navbar-list").css("width", "auto");
+		$(".navbar-list").css("padding", "0px 150px 0px 150px");
+		$(".navbar").css("width", "100%");
+		$(".center").css("padding", "30px 150px 30px 150px");
+		$("#intro").css("padding-top", "60px");
+		$(".navbar").css("font-size", "15px");
+		$(".menu").css("display", "none");
+		$(".menu").css("left", "0px");
+	}
 }
 
 function load(){
-	adjustPage();
-	if ($(window).width() <= 1148){
-		$(".navbar").hide();
-	}
+	adjustPage(navigator.platform, "onload");
 }
 
 function show(){
